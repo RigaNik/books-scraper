24c08476c33966149c7c20b296cb60436cc9b159
Nikola Stekovic <nikola@konstrukt.rs>
2018-01-30 11:07:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# books-scraper
+# Books scraper
+
+Really simple books scraper for oreilly's free books written in 2 minutes.
+
+## Steps to take
+* `git clone https://github.com/RigaNik/books-scraper.git`
+* `cd books-scraper`
+* `npm install`
+* `node scraping.js`
---
+++ scraping.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var forEach = require('async-foreach').forEach;
+
+var types = ['business', 'data', 'design', 'iot', 'programming', 'security', 'web-ops', 'web-platform'];
+
+forEach(types, function(item) {
+  var baseurl = 'http://www.oreilly.com/' + item + '/free/';
+
+  request(baseurl, function(err, res) {
+
+    if(!err && res.statusCode === 200) {
+      scrapeData(res, item, baseurl);
+    }
+  }); 
+})
+
+function scrapeData(res, folderName, getUrl) {
+  var $ = cheerio.load(res.body);
+  var links = $('.product-row a');
+  for(var i = 0, len = links.length; i < len; i++) {
+    var cleanLink = links[i].attribs.href.split('/');
+    var getFileName = cleanLink[cleanLink.length-1];
+    // strip query parameters
+    var removedQuestionMarks = getFileName.split('?');
+    var cleanedLink = removedQuestionMarks[0].replace('.csp', '.pdf');
+    if(!fs.existsSync("files/" + folderName)) {
+      fs.mkdirSync("files/" + folderName);
+    };
+    request(getUrl + 'files/' + cleanedLink).pipe(fs.createWriteStream('files/' + folderName + '/' + cleanedLink));
+  }
+}
